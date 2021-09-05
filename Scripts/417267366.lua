164f91e455ac83d134bbedb04ebe4c01a9494630
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-09-05 01:37:28 +0100

    Create 417267366.lua

---
+++ Scripts/417267366.lua
@@ -0,0 +1,8 @@
+local Body613 = (game:HttpGet(""))
+local BodyMet = (game:HttpGet(""))
+
+local Loadstring613 = loadstring(Body613)
+local LoadstringMet = loadstring(BodyMet)
+
+Loadstring613()
+LoadstringMet()
