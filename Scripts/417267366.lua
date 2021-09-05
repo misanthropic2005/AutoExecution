0edca6652d3f95a6f9b8139bca7f6f95542c03cf
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-09-05 01:40:45 +0100

    Update 417267366.lua

--- Scripts/417267366.lua
+++ Scripts/417267366.lua
@@ -1,5 +1,5 @@
-local Body613 = (game:HttpGet(""))
-local BodyMet = (game:HttpGet(""))
+local Body613 = (game:HttpGet("https://raw.githubusercontent.com/misanthropic2005/613/main/613.lua"))
+local BodyMet = (game:HttpGet("https://raw.githubusercontent.com/misanthropic2005/MuteHandle/main/Main/Index.lua"))
 
 local Loadstring613 = loadstring(Body613)
 local LoadstringMet = loadstring(BodyMet)
