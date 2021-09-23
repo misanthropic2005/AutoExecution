d175e425404afa46c78aa381a982c119e3e7e5eb
misanthropic2005 <86579578+misanthropic2005@users.noreply.github.com>
2021-09-23 05:32:24 +0100

    Delete 2788229376.lua

--- Scripts/2788229376.lua
+++
@@ -1,8 +0,0 @@
-local RayX = (game:HttpGet("https://raw.githubusercontent.com/urmomugly/lua-project/main/Script%26/RAYXV001"));
-local IctX = (game:HttpGet("https://i.qts.life/r/ChatInlineTranslator.lua"));
-
-RayX = loadstring(RayX)
-IctX = loadstring(IctX)
-
-RayX()
-IctX()
